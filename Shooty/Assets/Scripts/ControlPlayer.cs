8edc783f104519468c25f0c5470a754de1f26360
Bruce Weir <bruce.weir@bbc.co.uk>
2019-08-29 18:34:33 +0100

    Aircraft now rolls the correct way up when inverted

--- Shooty/Assets/Scripts/ControlPlayer.cs
+++ Shooty/Assets/Scripts/ControlPlayer.cs
@@ -4,8 +4,7 @@ using UnityEngine;
 
 public class ControlPlayer : MonoBehaviour
 {
-    // Start is called before the first frame update
-
+    
     public GenerateTerrain terrain;
     public float angularVelocity = 1f;
     public float acceleration = .1f;
@@ -13,27 +12,42 @@ public class ControlPlayer : MonoBehaviour
     public float turnRate = 1.0f;
     private float playerSpeed;
     private float currentAngle = 0f;
-    private GameObject child;
-
+    private GameObject player;
+    private GameObject playerRoll;
+    float inversionCountdownValue;
+    private bool performingRoll = false;
+    private float targetRollRotation  = 0;
+    private float totalRollRotation = 0;
+    private float rollSpeed = 90f;
+    
     void Start()
     {
-        playerSpeed = 0;
-        
-        SetChildPositions();
 
-        child = gameObject.transform.GetChild(0).gameObject;
-        Debug.Log("child: " + child.name);
+        playerSpeed = 5;
+        
+        player = gameObject.transform.GetChild(0).gameObject;
+        playerRoll = gameObject.transform.GetChild(0).GetChild(0).gameObject;
+
+        Debug.Log(playerRoll.name);
+
+        SetPlayerStartPositions();
+
+      //  StartCoroutine(InversionCountdown());
+
     }
 
-    void SetChildPositions()
+    void SetPlayerStartPositions()
     {
         
         Vector3 position = terrain.GetPosition(-gameObject.transform.rotation.eulerAngles.y);
-    //    position.y = 0.0f;
-        foreach(Transform child in transform)
-        {
-            child.position = position;
-        }
+        position.y = 0;
+        player.transform.position = position;
+
+        //player height is determined by height of the pivot arm to which it is apparently attached. 
+        float startHeight = terrain.heightScale + 3;
+
+        gameObject.transform.position = new Vector3(0, startHeight, 0);
+        
     }
 
     // Update is called once per frame
@@ -42,20 +56,26 @@ public class ControlPlayer : MonoBehaviour
         if(Input.GetKeyDown(KeyCode.Escape))
         {
             Application.Quit();
-        }
-        
+        }   
     }
     void FixedUpdate()
     {
-          
+        
+        if(PlayerUpsideDown() && performingRoll == false)
+        {
+            performingRoll = true;
+            totalRollRotation = 0.0f;
+            targetRollRotation = (playerRoll.transform.localEulerAngles.z + 180) % 360.0f;
+        }
+        
         if(Input.GetKey(KeyCode.D))
         {
             float angleChange = turnRate * Time.fixedDeltaTime;
 
             currentAngle += angleChange;
             
-            child.transform.Rotate(Vector3.right, angleChange);
-            
+            player.transform.Rotate(Vector3.right, angleChange);
+
         }
         if(Input.GetKey(KeyCode.A))
         {
@@ -63,10 +83,26 @@ public class ControlPlayer : MonoBehaviour
             
             currentAngle -= angleChange;
 
-            child.transform.Rotate(Vector3.right, -angleChange);            
+            player.transform.Rotate(Vector3.right, -angleChange);     
+    
         }
 
         currentAngle = currentAngle % 360f;
+
+        if(performingRoll)
+        {
+            float rollAmount = rollSpeed * Time.fixedDeltaTime;
+
+            playerRoll.transform.Rotate(Vector3.forward, rollSpeed * Time.fixedDeltaTime, Space.Self);
+
+            totalRollRotation += rollAmount;
+            
+            if(totalRollRotation > 180.0f)
+            {
+                playerRoll.transform.Rotate(Vector3.forward, 180.0f - totalRollRotation, Space.Self);
+                performingRoll = false;
+            }
+        }
 
         
         if(Input.GetKey(KeyCode.W)) //accelerate
@@ -100,4 +136,14 @@ public class ControlPlayer : MonoBehaviour
         
         gameObject.transform.position = position;
     }
+
+    private bool PlayerUpsideDown()
+    {      
+        if(playerRoll.transform.up.y > 0)
+        {
+            return false;
+        }
+        return true;
+    }
+
 }
