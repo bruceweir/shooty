d65662179eb27bb9494159f2170234d2cd49eae7
Bruce Weir <bruce.weir@bbc.co.uk>
2019-09-14 20:11:10 +0100

    added cloud pivot prefab

---
+++ Shooty/Assets/Scripts/CloudPivot.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudPivot : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float rotationSpeed;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        gameObject.transform.Rotate(Vector3.up, rotationSpeed * Time.fixedDeltaTime);
+    }
+}
